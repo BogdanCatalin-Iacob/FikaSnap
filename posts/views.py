1fa8e8132923da259b799e6b1602978116b05461
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-07-10 18:15:56 +0100

    Add post method

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Post
@@ -17,3 +18,15 @@ class PostList(APIView):
         serializer = PostSerializer(
             posts, many=True, context={'request': request})
         return Response(serializer.data)
+
+    def post(self, request):
+        '''
+        Create a new post with user's input
+        '''
+        serializer = PostSerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
