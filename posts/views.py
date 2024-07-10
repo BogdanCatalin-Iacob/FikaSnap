2a1497628321b5d5fb0f86501dd9511cf52b1e6f
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-07-10 18:52:30 +0100

    Create put method to update an owned post

--- posts/views.py
+++ posts/views.py
@@ -41,6 +41,10 @@ class PostDetails(APIView):
     permission_classes = [IsOwnerOrReadOnly]
 
     def get_object(self, pk):
+        '''
+        Check if an object exists,
+        Handle DoesNotExist error otherwise
+        '''
         try:
             post = Post.objects.get(pk=pk)
             self.check_object_permissions(self.request, post)
@@ -49,8 +53,24 @@ class PostDetails(APIView):
             raise Http404
 
     def get(self, request, pk):
+        '''
+        Get a post by id
+        '''
         post = self.get_object(pk)
         serializer = PostSerializer(
             post, context={'request': request}
         )
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        '''
+        Update a post by id
+        '''
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
