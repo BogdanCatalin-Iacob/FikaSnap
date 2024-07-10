4a183e2d71480dab78cfff2f1f5f466f4e37e83c
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-07-10 18:45:27 +0100

    Create PostDetail view with get method

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,10 @@
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Post
 from .serializers import PostSerializer
+from fikasnap_api.permissions import IsOwnerOrReadOnly
 
 
 class PostList(APIView):
@@ -32,3 +34,23 @@ class PostList(APIView):
             serializer.save(owner=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PostDetails(APIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_object(self, pk):
+        try:
+            post = Post.objects.get(pk=pk)
+            self.check_object_permissions(self.request, post)
+            return post
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post, context={'request': request}
+        )
+        return Response(serializer.data)
