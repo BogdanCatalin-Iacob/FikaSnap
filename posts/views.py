59a0b1960b9ccebfe8facf778031c2655d801772
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-07-10 18:14:52 +0100

    Create Post view and urls

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostList(APIView):
+    '''
+    List / Create posts
+    '''
+
+    def get(self, request):
+        '''
+        Lists all posts
+        '''
+        posts = Post.objects.all()
+        serializer = PostSerializer(
+            posts, many=True, context={'request': request})
+        return Response(serializer.data)
