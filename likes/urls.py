4f624fa4596a4a301dc3581ef448aeb9a3061a56
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-07-23 21:57:43 +0100

    Create LikeDetails generic view and urls

--- likes/urls.py
+++ likes/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('likes/', views.LikeList.as_view())
+    path('likes/', views.LikeList.as_view()),
+    path('likes/<int:pk>/', views.LikeDetails.as_view()),
 ]
--- likes/views.py
+++ likes/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from fikasnap_api.permissions import IsOwnerOrReadOnly
 from .models import Like
 from .serializers import LikeSerializer
 
@@ -10,3 +11,12 @@ class LikeList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
+
+
+class LikeDetails(generics.RetrieveDestroyAPIView):
+    '''
+    Retrieve a like or delete it by id if you own it
+    '''
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
