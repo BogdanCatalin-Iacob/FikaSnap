36f4622a0123c6ab68b0a77a3d5f18a90aadc708
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-07-23 01:59:26 +0100

    Create LikeList view and create method to check against duplicate likes

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,15 +1,28 @@
+from django.db import IntegrityError
 from rest_framework import serializers
 from .models import Like
 
 
 class LikeSerializer(serializers.ModelSerializer):
     '''
-    Serializer for Like model
+    Serializer for the Like model
+    The create method handles the unique constraint on "owner" and "post"
     '''
     owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Like
         fields = [
-            'id', 'created_at', 'owner',' post'
+            'id', 'created_at', 'owner', ' post'
         ]
+
+    def create(self, validated_data):
+        '''
+        Return complete object instance based on validateddata
+        '''
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
