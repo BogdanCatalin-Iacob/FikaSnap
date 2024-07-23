ccb9a688b357322380c90b8fb7d7985c8c03cb63
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-07-23 01:50:43 +0100

    Refactor fix LikeSerializer name

--- likes/serializers.py
+++ likes/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Like
 
 
-class LieSerializer(serializers.ModelSerializer):
+class LikeSerializer(serializers.ModelSerializer):
     '''
     Serializer for Like model
     '''
