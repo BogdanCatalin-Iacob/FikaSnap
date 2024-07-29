bf18fd167b5ae0ddc4924121f4fe36d0aa090612
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-07-29 13:10:30 +0100

    Refactor: ProfileDetail as generic view

--- profiles/views.py
+++ profiles/views.py
@@ -17,31 +17,10 @@ class ProfileList(generics.ListAPIView):
     queryset = Profile.objects.all()
 
 
-class ProfileDetail(APIView):
+class ProfileDetail(generics.RetrieveUpdateAPIView):
     '''
-    Get the details page of a profile
+    Retrieve or update a profile if you\'re the owner.
     '''
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serialzier = ProfileSerializer(profile, context={'request': request})
-        return Response(serialzier.data)
-
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Profile.objects.all()
