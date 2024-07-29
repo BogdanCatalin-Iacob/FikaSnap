beca2ad5edb462c098cd2cf1e065ae838e3113c1
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-07-29 16:22:57 +0100

    Remove unused imports

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,3 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
 from fikasnap_api.permissions import IsOwnerOrReadOnly
