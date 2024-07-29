4e184aa2b73125d414d3318497d746ee0c07e372
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-07-29 19:34:56 +0100

    Add corsheaders middleware, allowed hosts, jwt different site allowed

--- fikasnap_api/settings.py
+++ fikasnap_api/settings.py
@@ -50,6 +50,7 @@ REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'fikasnap_api.serializers.CurrentUserSerializer'
@@ -62,9 +63,13 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'https://fikasnap-app-d28b6f6fbe1b.herokuapp.com/']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'https://fikasnap-app-d28b6f6fbe1b.herokuapp.com/'
+    ]
 
 
 # Application definition
@@ -87,6 +92,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'dj_rest_auth.registration',
+    'corsheaders',
     'profiles',
     'posts',
     'comments',
@@ -97,6 +103,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -106,6 +113,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware'
 ]
+
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'fikasnap_api.urls'
 
