cf06f8ea24d127709222b1dd2ef72f68f2d95ab7
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-07-29 19:26:19 +0100

    Add Procfile and set the allowed hosts

--- fikasnap_api/settings.py
+++ fikasnap_api/settings.py
@@ -64,7 +64,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', 'https://fikasnap-app-d28b6f6fbe1b.herokuapp.com/']
 
 
 # Application definition
