e7e6382c6f154bf5c5654b0bd25170e462c2c1c2
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-08-19 19:26:01 +0100

    Refactor: change the wrong setting for dj-rest-auth

--- fikasnap_api/settings.py
+++ fikasnap_api/settings.py
@@ -47,12 +47,15 @@ if 'DEV' not in os.environ:
     ]
 
 USE_JWT = True
-REST_USE_JWT = True'
+REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
-USER_DETAILS_SERIALIZER ='fikasnap_api.serializers.CurrentUserSerializer'
+
+REST_AUTH_SERIALIZERS = {
+    'USER_DETAILS_SERIALIZER': 'fikasnap_api.serializers.CurrentUserSerializer'
+}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
