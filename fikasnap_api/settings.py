86c8b2566640143de2f7d21f4ef4af9f08e64bd9
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-09-12 21:08:51 +0100

    Refactor: update dj-rest-auth user  serializer variable

--- fikasnap_api/settings.py
+++ fikasnap_api/settings.py
@@ -60,10 +60,7 @@ REST_AUTH = {
     'JWT_AUTH_COOKIE': 'my-app-auth',
     'JWT_AUTH_REFRESH_COOKIE': 'my-refresh-token',
     'JWT_AUTH_SAMESITE': 'None',
-}
-
-REST_AUTH_SERIALIZERS = {
-    'USER_DETAILS_SERIALIZER': 'fikasnap_api.serializers.CurrentUserSerializer'
+    'USER_DETAILS_SERIALIZER': 'fikasnap_api.serializers.CurrentUserSerializer',
 }
 
 # Quick-start development settings - unsuitable for production
