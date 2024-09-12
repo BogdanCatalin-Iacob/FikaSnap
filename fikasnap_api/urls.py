fc681f8359f1bb94e46415283dbed4f25dd13ec7
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-09-12 21:05:02 +0100

    Refactor: fix typo on logout url

--- fikasnap_api/urls.py
+++ fikasnap_api/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth', include('rest_framework.urls')),
     # custom logout must be before default to match first
-    path("('dj-rest-auth/logout/", logout_route),
+    path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include(
         'dj_rest_auth.registration.urls')),
